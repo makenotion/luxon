29369290b676ea3877850742ed96cec406f5d29f
Ben Hughes <benjamin@makenotion.com>
2023-12-22 14:08:24 -0500

    Add DateTime.buildFormatParser and DateTime.fromFormatParser
    This allows constructing a parser for a locale/format and reusing it
    when parsing dates. Without this, DateTime.fromFormat constructs a new
    parser on every call. When parsing large amounts of date strings, this
    gets rather slow.
    
    In benchmarks, this speeds up parsing by 4.4x

--- test/datetime/tokenParse.test.js
+++ test/datetime/tokenParse.test.js
@@ -1141,3 +1141,28 @@ test("DateTime.parseFormatForOpts returns a parsing format", () => {
   const format = DateTime.parseFormatForOpts("");
   expect(format).toBeNull();
 });
+
+//------
+// .fromFormatParser
+//-------
+
+test("DateTime.fromFormatParser behaves equivalently to DateTime.fromFormat", () => {
+  const dateTimeStr = "1982/05/25 09:10:11.445";
+  const format = "yyyy/MM/dd HH:mm:ss.SSS";
+  const formatParser = DateTime.buildFormatParser(format);
+  const ff1 = DateTime.fromFormat(dateTimeStr, format),
+    ffP1 = DateTime.fromFormatParser(dateTimeStr, formatParser);
+
+  expect(ffP1).toEqual(ff1);
+  expect(ffP1.isValid).toBe(true);
+});
+
+test("DateTime.fromFormatParser throws error when used with a different locale than it was created with", () => {
+  const format = "yyyy/MM/dd HH:mm:ss.SSS";
+  const formatParser = DateTime.buildFormatParser(format, { locale: "es-ES" });
+  expect(() =>
+    DateTime.fromFormatParser("1982/05/25 09:10:11.445", formatParser, { locale: "es-MX" })
+  ).toThrowError(
+    "fromFormatParser called with a locale of Locale(es-MX, null, null), but the format parser was created for Locale(es-ES, null, null)"
+  );
+});
